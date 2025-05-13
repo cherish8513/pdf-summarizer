ea377e59482233a16049910c2dfff89442684a84
jooheon <cherish8513>
2025-05-13 16:42:16 +0900

    Add dummy summarize api

---
+++ src/pdf_summarizer/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.pdf_summarizer.routers import summarize
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:8080"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(summarize.router, prefix="/api")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
---
+++ src/pdf_summarizer/routers/summarize.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import JSONResponse
+import os
+import tempfile
+
+from src.pdf_summarizer.models.schemas import SummaryResponse
+
+router = APIRouter()
+
+
+@router.post("/api/summarize", response_model=SummaryResponse)
+async def summarize(file: UploadFile = File(requried = True)):
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
+            contents = await file.read()
+            tmp.write(contents)
+            temp_path = tmp.name
+        # TODO: pdf to text
+        # TODO: sLLM 연동
+        return {
+            "title": "요약 결과",
+            "summary": "이 문서는 학습 노트 생성을 위한 테스트 문서입니다.",
+            "keywords": ["요약", "노트", "FastAPI"]
+        }
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+    finally:
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
