cbdf4cf27c312bc9a10dfee8d4dde45c236c3475
jooheon <cherish8513>
2025-05-28 15:49:23 +0900

    change to rage client from prompt

--- src/llm/pdf_rag_client.py
+++ src/llm/pdf_rag_client.py
@@ -7,6 +7,14 @@ from langsmith import traceable
 from llm.config import config
 
 
+class PdfNotLoadedError(Exception):
+    pass
+
+
+class PdfProcessingError(Exception):
+    pass
+
+
 class PdfRagClient:
     def __init__(self, llm, pdf_loader, embeddings):
         self.retriever = None
@@ -16,26 +24,41 @@ class PdfRagClient:
 
     @traceable(name="pdf_load", tags=["pdf", "vectorize"])
     async def pdf_load(self, file_path: str):
-        docs = self.pdf_loader.load_and_split_pdf(file_path)
-        vector_store = FAISS.from_documents(documents=docs, embedding=self.embeddings)
-        self.retriever = vector_store.as_retriever(
-            search_type=config.search_type,
-            search_kwargs={"k": config.default_k}
-        )
+        self.retriever = None
+        try:
+            docs = self.pdf_loader.load_and_split_pdf(file_path)
+
+            if not docs:
+                raise PdfProcessingError(f"'{file_path}' could not load or split")
+
+            vector_store = FAISS.from_documents(documents=docs, embedding=self.embeddings)
+            current_retriever = vector_store.as_retriever(
+                search_type=config.search_type,
+                search_kwargs={"k": config.default_k}
+            )
+            self.retriever = current_retriever
+        except PdfProcessingError as pe:
+            raise pe
+        except Exception as e:
+            raise PdfProcessingError(
+                f"pdf '{file_path}' unexpected error occurred: {e}") from e  # 원본 예외(e)를 포함하여 예외 체이닝
 
     @traceable(name="invoke", tags=["query", "llm"])
     async def invoke(self, question: str) -> str:
+        if self.retriever is None:
+            raise PdfNotLoadedError("PDF file not loaded.")
+
         prompt = PromptTemplate.from_template(
             """You are an assistant for question-answering tasks.\n
             Use the following pieces of retrieved context to answer the question.\n
             If you don't know the answer, just say that you don't know. \n
             Answer in Korean.\n
-            
+
             #Question: 
             {question} 
             #Context: 
             {context}
-            
+
             #Answer: 
             """
         )
